6420917d451335954fe587fc9ff8d7780d748036
Ankur Das <55151728+TaShUOP@users.noreply.github.com>
2021-07-25 16:41:28 +0530

    Update ping.js
    Command added

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 const Discord = require('discord.js')
+
 const fs = require('fs')
+
 const client = new Discord.Client()
 
 const data = fs.readFileSync('token.txt', 'utf8')
+
+const prefix = '-'
+
+client.commands = new Discord.Collection()
+
+const commandFiles = fs
+  .readdirSync('./commands/')
+  .filter((file) => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+
+  client.commands.set(command.name, command)
+}
 
 client.once('ready', () => {
   console.log('bot ready!')
 })
 
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+
+  if (command === 'ping') {
+    client.commands.get('ping').execite(message, args)
+  }
+})
 client.login(data)
