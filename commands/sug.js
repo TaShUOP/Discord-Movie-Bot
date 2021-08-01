20b0c62e0015df152d1c8e79214345e493f75211
Ankur Das <55151728+TaShUOP@users.noreply.github.com>
2021-08-01 18:07:41 +0530

    Update Sug.js

--- commands/sug.js
+++ commands/sug.js
@@ -2,6 +2,10 @@ module.exports = {
   name: 'sug',
   description: 'See the suggestion list',
   execute(message, args) {
-    message.channel.send('sug')
+    if (message.member.roles.cache.has('868846077791576105')) {
+      message.channel.send('sug')
+    } else {
+      message.channel.send("You don't have the right permissions.")
+    }
   },
 }
